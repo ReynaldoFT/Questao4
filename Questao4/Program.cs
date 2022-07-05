75e190aa1d088a9a01232097d5045d4c7d806e55
Naja Info <Naja Info@NALDINHO>
2022-07-05 00:19:48 -0300

    Adicionar arquivos de projeto.

---
+++ Questao4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Questao4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+
+            for(numero = 1; numero <=50; numero++)
+            {
+                Console.WriteLine(numero);
+            }
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
